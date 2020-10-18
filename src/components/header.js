06927b9e19d898404fee9bbde5630acd6597e566
Tom <tlaryea@ukcloud.com>
2020-10-18 11:08:12 +0100

    Refresh

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,99 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+ FormGroup,
+ Label,
+ Input
+} from 'reactstrap';
+import logo from '../images/logo-main-white.png';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+  
+  // const [search,setSearch] = useState(false)
+
+  const submitSearch = (e) =>{
+      e.preventDefault();
+      alert("Searched");
+  }
+
+  // const openSearch = () =>{
+  //     setSearch(true);
+
+
+  // }
+
+  // const searchClass = search ?'searchInput active': 'searchInput'; 
+  return (
+    <div>
+      <Navbar id="pageNav" dark expand={true} fixed="top" color="black">
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/about">About</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/contact">Contact</NavLink>
+            </NavItem>
+          </Nav>
+          <FormGroup id="searchForm" onSubmit={submitSearch}>
+            <Input
+              type="search"
+              name="search"
+              id="searchBox"
+              placeholder="search"
+            />
+            <img className="searchIcon" src={require('../images/search.png')} alt="Search"/>
+          </FormGroup>
+      </Navbar>
+      <div className="hero">
+        <div className="content">
+          <img src={logo}></img>
+          <h2>Find the interesting in existing,
+            the extraodinary from ordinary.
+          </h2>
+        </div>
+      </div>
+      <Navbar id="catNav" light expand="md">
+        <NavbarToggler id="hamburger" onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="m-auto" navbar>
+            <NavItem>
+              <NavLink href="/">Politics</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/">Culture</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/contact">Travel</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/">Technology</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/about">Business</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/contact">Health</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
     </div>
-  </header>
-)
+  );
+}
+
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,11 +3,11 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const ContactPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Home Page</h1>
+    <SEO title="Contact" />
+    <h1>Contact Page</h1>
   </Layout>
 )
 
-export default IndexPage
+export default ContactPage
